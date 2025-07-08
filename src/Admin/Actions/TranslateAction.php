e05f91fae76659f289481a3add40d699ff2f8f6d
maxboyko <maxboiko@divotek.com>
2025-07-08 12:37:34 +0300

    small fixes

--- src/Admin/Actions/TranslateAction.php
+++ src/Admin/Actions/TranslateAction.php
@@ -12,18 +12,21 @@ class TranslateAction
     public static function make(Schema $schema, string $field, string $fieldClass, bool $required = false): Action
     {
         return Action::make('translate')->icon(Heroicon::OutlinedLanguage)->schema(function () use ($fieldClass, $required): array {
-            return lang()->adminLanguages()->filter(fn ($language) => $language->slug !== main_lang())->map(function ($language) use ($fieldClass, $required) {
+            return lang()->adminLanguages()->filter(fn($language) => $language->slug !== main_lang())->map(function ($language) use ($fieldClass, $required) {
                 return $fieldClass::make($language->slug)->label($language->name)->required($required);
             })->toArray();
         })->fillForm(function () use ($schema, $field): array {
+            /**@phpstan-ignore-next-line */
             return $schema->getRecord()->seo()->get()->mapWithKeys(function ($seo) use ($field) {
                 return [
                     $seo->language->slug => $seo?->$field,
                 ];
             })->toArray();
         })->action(function (array $data) use ($schema, $field): void {
+            $record = $schema->getRecord();
             foreach ($data as $language => $value) {
-                $schema->getRecord()->seo()->updateOrCreate(['language_id' => lang()->getBySlug($language)->id], [$field => $value]);
+                /**@phpstan-ignore-next-line */
+                $record->seo()->updateOrCreate(['language_id' => lang()->getBySlug($language)->id], [$field => $value]);
             }
             Notification::make()->success()->title(__('filament-panels::resources/pages/edit-record.notifications.saved.title'))->send();
         })->hidden(lang()->adminLanguages()->count() == 1);
--- src/Seo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace SmartCms\Seo;
-
-class Seo {}

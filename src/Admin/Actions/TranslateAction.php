e939e84c9f65f4bdcf9c5ee6c9cccd5c312d4086
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-07-08 09:37:54 +0000

    Fix styling

--- src/Admin/Actions/TranslateAction.php
+++ src/Admin/Actions/TranslateAction.php
@@ -12,7 +12,7 @@ class TranslateAction
     public static function make(Schema $schema, string $field, string $fieldClass, bool $required = false): Action
     {
         return Action::make('translate')->icon(Heroicon::OutlinedLanguage)->schema(function () use ($fieldClass, $required): array {
-            return lang()->adminLanguages()->filter(fn($language) => $language->slug !== main_lang())->map(function ($language) use ($fieldClass, $required) {
+            return lang()->adminLanguages()->filter(fn ($language) => $language->slug !== main_lang())->map(function ($language) use ($fieldClass, $required) {
                 return $fieldClass::make($language->slug)->label($language->name)->required($required);
             })->toArray();
         })->fillForm(function () use ($schema, $field): array {
